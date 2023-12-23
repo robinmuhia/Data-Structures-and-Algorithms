b0c6e25e9998f3eab1bad1a4e2d1f84c697b398a
robinmuhia <muhiarobinonyancha@gmail.com>
2023-12-23 12:36:08 +0300

    feat: Introduce readme and refactor rps
    This commit will add a readme and refactor the rock, paper and scissors game

--- noob-projects/rps.py
+++ noob-projects/rps.py
@@ -1,57 +1,100 @@
 import random
 
-computer_choices = ("rock", "paper", "scissors")
-player_wins = 0
-computer_wins = 0
-draws = 0
-is_playing = True
 
-name = input("Name: ").title()
-print(
-    """Please type :
-R-to choose rock
-P-to choose paper
-S-to choose scissors
-Q-to quit the game"""
-)
+class RockPaperScissors:
+    """Class that encapsulates core logic of rock, paper, scissors game"""
 
-while is_playing:
-    command = input("> ").lower()
-    if command == "q":
-        is_playing = False
+    def __init__(self) -> None:
+        """Construtor for this class."""
+        self.computer_choices = ("rock", "paper", "scissors")
+        self.wins = 0
+        self.losses = 0
+        self.draws = 0
+        self.name = ""
+        self.player_choices = ("r", "p", "s", "q")
+
+    def initialize_game(self) -> None:
+        """Initialize the game."""
+        name = input("name: ").title()
+        print(
+            """Please type :
+        R-to choose rock
+        P-to choose paper
+        S-to choose scissors
+        Q-to quit the game"""
+        )
+        self.name = name
+
+    @staticmethod
+    def get_player_command() -> str:
+        """Get the player input."""
+        player_command = input("> ").lower()
+        return player_command
+
+    def validate_player_input(self, player_command: str) -> bool:
+        """Validate player chooses only r, p, s or q."""
+        if player_command not in self.player_choices:
+            print("Please input 'r','p' or 's' to play. Press 'q' to quit!")
+            return False
+        return True
+
+    def end_game(self) -> None:
+        """Ends the game and renders the stats"""
         print("Thanks for playing!")
-        print(f"You won {player_wins} times")
-        print(f"Computer won {computer_wins} times")
-        print(f"You drew {draws} times with the computer")
-        if player_wins > computer_wins:
-            print(f"Congratulations,{name}! You defeated the computer!")
-        elif computer_wins > player_wins:
-            print(f"You suck,{name}! Computer won more times!")
+        print(f"You won {self.wins} times")
+        print(f"Computer won {self.losses} times")
+        print(f"You drew {self.draws} times with the computer")
+        if self.wins > self.losses:
+            print(f"Congratulations,{self.name}! You defeated the computer!")
+        elif self.losses > self.wins:
+            print(f"You suck,{self.name}! Computer won more times!")
         else:
             print("Game ends in a draw!")
-        break
-    possibilities = random.choice(computer_choices)
-    print(f"Computer picked {possibilities}")
-    if (
-        (command == "r" and possibilities == "scissors")
-        or (command == "p" and possibilities == "rock")
-        or (command == "s" and possibilities == "paper")
-    ):
-        player_wins += 1
-        print("You won!")
-    elif (
-        (command == "r" and possibilities == "paper")
-        or (command == "p" and possibilities == "scissors")
-        or (command == "s" and possibilities == "rock")
-    ):
-        computer_wins += 1
-        print("You lose!")
-    elif (
-        (command == "r" and possibilities == "rock")
-        or (command == "p" and possibilities == "paper")
-        or (command == "s" and possibilities == "scissors")
-    ):
-        draws += 1
-        print("Draw!")
-    else:
-        print("Please input 'r','p' or 's' to play. Press 'q' to quit!")
+
+    def check_game_status(self, player_command: str) -> bool:
+        """Check the game status incase the player quits."""
+        if player_command == "q":
+            return False
+        return True
+
+    def get_computer_move(self) -> str:
+        computer_move = random.choice(self.computer_choices)
+        return computer_move
+
+    def game_logic(self, player_command: str, computer_move: str) -> None:
+        """The core logic of this game"""
+        print(f"Computer chose {computer_move}")
+        if (
+            (player_command == "r" and computer_move == "scissors")
+            or (player_command == "p" and computer_move == "rock")
+            or (player_command == "s" and computer_move == "paper")
+        ):
+            self.wins += 1
+            print("You won!")
+        elif (
+            (player_command == "r" and computer_move == "paper")
+            or (player_command == "p" and computer_move == "scissors")
+            or (player_command == "s" and computer_move == "rock")
+        ):
+            self.losses += 1
+            print("You lose!")
+        else:
+            self.draws += 1
+            print("Draw!")
+
+    def play(self) -> None:
+        self.initialize_game()
+        while True:
+            player_command = self.get_player_command()
+            is_move_valid = self.validate_player_input(player_command=player_command)
+            if not is_move_valid:
+                continue
+            game_status = self.check_game_status(player_command=player_command)
+            if not game_status:
+                self.end_game()
+                break
+            computer_move = self.get_computer_move()
+            self.game_logic(player_command=player_command, computer_move=computer_move)
+
+
+RockPaperScissors().play()
